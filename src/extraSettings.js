536e0d2edee64f2737968f648404fa305c7cca7e
pandecheng <pandecheng36@gmail.com>
2021-02-02 19:42:36 +0800

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -3,7 +3,7 @@
  */
 
 // 额外分数：修改数字
-let extraScore = 1;
+let extraScore = 10;
 
 // 无敌模式：true 改为 false
 let wuDi = true;
@@ -26,7 +26,7 @@ let setFruits = {
 }
 
 // 让水果更 Q 弹：false 改为大于 0 小于 1 的任意小数（推荐 0.9）
-let fruitQTan = false;
+let fruitQTan = 0.2;
 
 // 让水果下落缓慢：false 改为大于 0 的任意数，值越大阻力越大，下落越慢（推荐 5）
 let fruitSlowDown = false;
@@ -35,10 +35,10 @@ let fruitSlowDown = false;
 let clickChangeFruit = false;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
+let adLink = 'http://qm.qq.com/cgi-bin/qm/qr?k=EGVYAI_-JqHrh1rsA-vJJ_EzkEPLLULl';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
+document.getElementsByTagName("title")[0].innerText = '合成大表盘 - pandecheng';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
